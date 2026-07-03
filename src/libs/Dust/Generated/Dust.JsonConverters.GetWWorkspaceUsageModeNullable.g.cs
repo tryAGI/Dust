25f81116abf127e1d0e28d1c80873fd76830fb90
Konstantin S. <havendv@gmail.com>
2026-07-03 18:08:55 +0400

    feat: Updated OpenAPI spec (#88)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Dust/Generated/Dust.JsonConverters.GetWWorkspaceUsageModeNullable.g.cs
@@ -0,0 +1,60 @@
+#nullable enable
+
+namespace Dust.JsonConverters
+{
+    /// <inheritdoc />
+    public sealed class GetWWorkspaceUsageModeNullableJsonConverter : global::System.Text.Json.Serialization.JsonConverter<global::Dust.GetWWorkspaceUsageMode?>
+    {
+        /// <inheritdoc />
+        public override global::Dust.GetWWorkspaceUsageMode? Read(
+            ref global::System.Text.Json.Utf8JsonReader reader,
+            global::System.Type typeToConvert,
+            global::System.Text.Json.JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case global::System.Text.Json.JsonTokenType.String:
+                {
+                    var stringValue = reader.GetString();
+                    if (stringValue != null)
+                    {
+                        return global::Dust.GetWWorkspaceUsageModeExtensions.ToEnum(stringValue);
+                    }
+                    
+                    break;
+                }
+                case global::System.Text.Json.JsonTokenType.Number:
+                {
+                    var numValue = reader.GetInt32();
+                    return (global::Dust.GetWWorkspaceUsageMode)numValue;
+                }
+                case global::System.Text.Json.JsonTokenType.Null:
+                {
+                    return default(global::Dust.GetWWorkspaceUsageMode?);
+                }
+                default:
+                    throw new global::System.ArgumentOutOfRangeException(nameof(reader));
+            }
+
+            return default;
+        }
+
+        /// <inheritdoc />
+        public override void Write(
+            global::System.Text.Json.Utf8JsonWriter writer,
+            global::Dust.GetWWorkspaceUsageMode? value,
+            global::System.Text.Json.JsonSerializerOptions options)
+        {
+            writer = writer ?? throw new global::System.ArgumentNullException(nameof(writer));
+
+            if (value == null)
+            {
+                writer.WriteNullValue();
+            }
+            else
+            {
+                writer.WriteStringValue(global::Dust.GetWWorkspaceUsageModeExtensions.ToValueString(value.Value));
+            }
+        }
+    }
+}
